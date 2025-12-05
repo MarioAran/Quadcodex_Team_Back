4d078bfca9be1039d6faccf2da3626f7abef889d
Gabriel Michielon <gabrielmichelon1612@gmail.com>
2025-12-05 16:14:38 +0100

    Env para todos

--- config/new-env.sh
+++ config/new-env.sh
@@ -3,10 +3,15 @@
 ENV_PATH="../app/.env"
 if [ ! -d "$ENV_PATH" ]; then
     echo "Creando entorno virtual en $ENV_PATH..."
-    python3.10 -m venv "$ENV_PATH"
+    python -m venv "$ENV_PATH"
 fi
 echo "Activando entorno virtual..."
-source "$ENV_PATH/bin/activate"
+# Detectar si es Windows o Linux/Mac
+if [ -f "$ENV_PATH/Scripts/activate" ]; then
+    source "$ENV_PATH/Scripts/activate"
+else
+    source "$ENV_PATH/bin/activate"
+fi
 
 echo "Instalando dependencias..."
 pip install --upgrade pip
